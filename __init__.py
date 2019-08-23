a006536cb02540618e422ff418c082785dad0e47
nanhongliu <hongnanliu@creditease.cn>
2019-08-23 14:02:54 +0800

    first commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liuhongnan
+@contact: liunanmu@163.com
+@Blog address:http://www.cnblogs.com/liuhongnan/
+@File    : __init__.py.py
+@Time    : 2019/8/23 11:41 AM
+'''
---
+++ 函数.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liuhongnan
+@contact: liunanmu@163.com
+@Blog address:http://www.cnblogs.com/liuhongnan/
+@File    : 函数.py
+@Time    : 2019/8/23 11:41 AM
+'''
+username=input('username:')
+password=input('password:')
+if username == 'seven' and password == '123':
+    print('登陆成功')
+
+else:
+    print('登陆失败')
